93a438cd0a39de35aa688afe0a1d4a058ab41773
Stephan <4.da.crap@gmail.com>
2024-05-22 13:12:10 +0200

    asdasd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation  } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app.routes';
 import { addKeyInterceptor } from './core/interceptors/add-key.interceptor';
@@ -7,7 +7,7 @@ import { AuthGuard } from './core/guards/authGuard';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-      provideRouter(routes, withHashLocation()),
+      provideRouter(routes),
       provideHttpClient(withFetch()),
       provideHttpClient(withInterceptors([addKeyInterceptor])),
       AuthGuard
