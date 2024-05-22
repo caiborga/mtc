03dc0fb46f13623440ecf19e22faf0f2126aebfc
Stephan <4.da.crap@gmail.com>
2024-05-22 08:49:03 +0200

    asdasd

--- src/redirect.js
+++ src/redirect.js
@@ -3,8 +3,12 @@
   var currentPath = window.location.pathname;
   var indexPath = baseHref + 'index.html';
 
-  // Überprüfen, ob die aktuelle Seite die 404-Seite ist
-  if (currentPath !== indexPath && !currentPath.endsWith('/404.html')) {
+  // Überprüfen, ob die aktuelle Seite die 404-Seite ist und nicht die index.html
+  if (!currentPath.startsWith(baseHref)) {
+    // Dies ist eine Sicherheitsprüfung, um sicherzustellen, dass wir nur innerhalb des Basispfads weiterleiten.
+    window.location.replace(baseHref);
+  } else if (currentPath !== indexPath && !currentPath.endsWith('/404.html')) {
+    // Falls der aktuelle Pfad nicht die index.html ist und auch nicht die 404.html ist, leiten wir weiter.
     var newPath = baseHref + '#' + currentPath.substring(baseHref.length);
     window.location.replace(newPath);
   }
